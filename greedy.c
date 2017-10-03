dedd5530b852a65f14f68892f2af069b330e8698
Jonetcetera <jon.charles.artner@gmail.com>
2017-10-03 00:04:41 -0700

    Minimum coins per transaction

---
+++ greedy.c
@@ -0,0 +1,61 @@
+/**
+ *
+Finds the minimum number of coins to make change.
+ *
+ */
+
+ #include <stdio.h>
+ #include <math.h>
+ #include "cs50.h"
+
+ int main(void)
+ {
+     float n;
+     int count = 0, amount;
+
+     // prompts user for amount owed and validates input
+     do
+     {
+         printf("How much change is owed?\n");
+         n = get_float();
+     }
+     while (n < 0);
+
+
+    // convert input into cents
+     n *= 100.0;
+     amount = (int) round(n);
+
+     // while quarter can be used
+     while (amount >= 25)
+     {
+         count++;
+         amount -= 25;
+     }
+
+     // while dimes can be used
+     while (amount >= 10)
+     {
+         count++;
+         amount -= 10;
+     }
+
+     // while nickels can be used
+     while (amount >= 5)
+     {
+         count++;
+         amount -= 5;
+     }
+
+     // while pennies can be used
+     while (amount >= 1)
+     {
+         count++;
+         amount -= 1;
+     }
+
+     // prints the minimum number of coins for change
+     printf("%d", count);
+
+     return 0;
+ }
